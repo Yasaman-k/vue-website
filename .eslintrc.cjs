51b241bd45368cd1e57a0a4f2bd3f7b5a240c366
Yasaman K <yasaman@m.lo.agency>
2022-06-08 21:50:13 +0430

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,20 +6,9 @@ module.exports = {
   "extends": [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier"
   ],
   "env": {
     "vue/setup-compiler-macros": true
-  },
-  "overrides": [
-    {
-      "files": [
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
-      ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
+  }
 }
